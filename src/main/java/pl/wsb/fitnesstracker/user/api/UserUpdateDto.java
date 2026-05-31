13755e59ccf6172b625eb6010481b902588d9e7a
vdi-terminal <ekafrozennn@gmail.com>
2026-05-31 14:46:22 +0200

    Dodanie plikow Usera

---
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserUpdateDto.java
@@ -0,0 +1,11 @@
+package pl.wsb.fitnesstracker.user.api;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+
+public record UserUpdateDto(
+        String firstName,
+        String lastName,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate birthdate,
+        String email
+) {}
