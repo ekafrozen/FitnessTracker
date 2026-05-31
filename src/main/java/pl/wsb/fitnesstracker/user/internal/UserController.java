12b4931561ac79617037baf144ce7db619b259a3
vdi-terminal <ekafrozennn@gmail.com>
2026-05-31 15:27:26 +0200

    Edycja plikow 2

--- src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,32 +1,87 @@
-package pl.wsb.fitnesstracker.user.internal;
+package pl.wsb.fitnesstracker.user.api;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pl.wsb.fitnesstracker.user.api.UserDto;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * UserController is responsible for handling HTTP requests related to user operations.
- * It provides endpoints for retrieving and creating users.
- */
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/v1/users")
-@RequiredArgsConstructor
 class UserController {
 
-    private final UserServiceImpl userService;
+    private final UserService userService;
+    private final UserProvider userProvider;
 
-    private final UserMapper userMapper;
-
-    @PostMapping
-    public UserDto addUser(@RequestBody UserDto userDto) throws InterruptedException {
-
-        // TODO: Implement the method to add a new user.
-        //  You can use the @RequestBody annotation to map the request body to the UserDto object.
-
-        return null;
+    UserController(UserService userService, UserProvider userProvider) {
+        this.userService = userService;
+        this.userProvider = userProvider;
     }
 
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userProvider.findAllUsers().stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/simple")
+    public List<UserBasicDto> getSimpleUsers() {
+        return userProvider.findAllUsers().stream()
+                .map(user -> new UserBasicDto(user.getId(), user.getFirstName(), user.getLastName()))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return userProvider.getUser(id)
+                .map(this::mapToDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/email")
+    public List<UserEmailDto> searchByEmail(@RequestParam String email) {
+        return userProvider.searchUsersByEmailFragment(email).stream()
+                .map(user -> new UserEmailDto(user.getId(), user.getEmail()))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/older/{time}")
+    public List<UserDto> searchOlderThan(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate time) {
+        return userProvider.findAllUsers().stream()
+                .filter(user -> user.getBirthdate().isBefore(time))
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        User user = new User(userDto.firstName(), userDto.lastName(), userDto.birthdate(), userDto.email());
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapToDto(createdUser));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserUpdateDto updateDto) {
+        try {
+            User updatedUser = userService.updateUser(userId, updateDto);
+            return ResponseEntity.ok(mapToDto(updatedUser));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
+
+    private UserDto mapToDto(User user) {
+        return new UserDto(user.getId(), user.getFirstName(), user.getLastName(), user.getBirthdate(), user.getEmail());
+    }
 }
